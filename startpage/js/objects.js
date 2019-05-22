77aa1461c9262f08f5bb1987b9b42fcf6cc70cac
adriandersen <adriandersen3@gmail.com>
2019-05-22 21:11:07 +0200

    added object

---
+++ startpage/js/objects.js
@@ -0,0 +1,32 @@
+let obj = [
+    Google = {
+        name:"Google",
+        url:"google.com",
+        short:"g",
+        links: [
+            Drive = {
+                name:"Drive",
+                url:"https://drive.google.com/drive/u/0/my-drive",
+                short:"d"
+            }
+        ]
+    },
+    Reddit = {
+        name:"Reddit",
+        url:"reddit.com",
+        short:"r",
+
+        links: [
+            Norge = {
+                name:"Norge",
+                url:"reddit.com/r/norge",
+                short:"n"
+            }
+        ]
+    },
+    Netflix = {
+        name:"Netflix",
+        short:"n",
+        
+    }
+]
