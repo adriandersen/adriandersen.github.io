08dd5fd4fefa5295a6d8354958fcde8f11073351
adriandersen <adriandersen3@gmail.com>
2019-05-23 14:39:15 +0200

    Added some unreadable arrow-function-code. Will comment later

--- startpage/js/src.js
+++ startpage/js/src.js
@@ -3,8 +3,12 @@ console.log(obj[1]);
 
 document.addEventListener("keypress", event => { 
     console.log(event.key);
-    
-
-
+    if(obj){
+        if(obj.forEach( item => { 
+            if (item.short == event.key){
+                console.log("Hit!");
+            }
+        }));
+    }
 
 })
