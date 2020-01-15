b6e9aa5b4576139ec60e9f650cd96691ffb58bdc
Adrian Andersen <adriandersen3@gmail.com>
2020-01-15 09:51:21 +0100

    Started developement

--- startpage/src/App.js
+++ startpage/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class SearchField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { 
+      currentValue: 'Search' 
+    };
+  }
+
+
+  handleChange = event => {
+    this.setState({currentValue: event.target.value})
+
+
+  }
+
+  render() {
+
+    
+
+    return (
+      <div className="c">
+        <a id="logo" ><i className="g fab"></i></a>
+        <header className="App-header">
+          <form id="searchform" className="search" action="" method="" onSubmit={this.doSearch}>
+            <input className="search" id="input" type="text" placeholder="Search" onChange={this.handleChange} autoFocus></input>
+          </form>
+
+        </header>
+      </div>
+    );
+
+  }
+
 }
 
-export default App;
+
+
+
+export default SearchField;
